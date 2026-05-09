55e91b8d7f23223130d876b9dad10898241cb504
EclipseDev <adi.laviso2004@gmail.com>
2026-05-10 01:43:05 +0200

    update footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,6 +98,9 @@ export default function RootLayout({
   <main className="flex-1">
     {children}
   </main>
+<footer className="mt-20 py-6 text-center text-sm text-slate-500">
+  Built with Next.js & Supabase by Adrien Laviso
+</footer>
 
 </body>
 </html>
